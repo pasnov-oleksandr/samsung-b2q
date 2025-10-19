9be21b52982aec23fe2558efe47b90ea305f2323
Azkali <a.ffcc7@gmail.com>
2025-10-19 18:36:56 +0700

    build.sh: remove docker; remove usrmerge variant

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -xe
-if [[ $1 = "docker" ]]; then
-	docker build -t fold .
-	docker run --privileged --rm -it -v "$PWD":/build fold
-	exit
-fi
-
 [ -d build ] || git clone https://gitlab.com/ubports/porting/community-ports/halium-generic-adaptation-build-tools build
 ./build/build.sh -b . "$@"
-./build/prepare-fake-ota.sh ./out/device_q2q_usrmerge.tar.xz ota
+./build/prepare-fake-ota.sh ./out/device_q2q.tar.xz ota
 ./build/system-image-from-ota.sh ota/ubuntu_command out
 mv out/rootfs.img out/ubuntu.img
