0efbecfe10469116a9390ae964ee287e79f58194
vpnusageo3-oss <vpnusageo3@gmail.com>
2026-07-14 12:34:12 +0200

    device: b2q: adapt build structures, deviceinfo and configs from q2q template

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 set -xe
 [ -d build ] || git clone https://gitlab.com/ubports/porting/community-ports/halium-generic-adaptation-build-tools build
 ./build/build.sh -b workdir "$@"
-./build/prepare-fake-ota.sh ./out/device_q2q.tar.xz ota
+./build/prepare-fake-ota.sh ./out/device_b2q.tar.xz ota
 ./build/system-image-from-ota.sh ota/ubuntu_command out
 mv out/rootfs.img out/ubuntu.img
